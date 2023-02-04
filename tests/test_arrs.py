672e90a7acadcce8b75a32153b38ffeb092fd64f
Dmitry <k1791@mail.ru>
2023-02-04 15:04:23 +0500

    добавлен функционал фикстуры

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,13 +1,18 @@
+import pytest
 from utils import arrs
 
-def test_get():
-   assert arrs.get([1, 2, 3], 1, "test") == 2
+@pytest.fixture
+def list_():
+   return [1, 2, 3]
+
+def test_get(list_):
+   assert arrs.get(list_, 1, "test") == 2
    assert arrs.get([], 0, "test") == "test"
 
 
-def test_slice():
+def test_slice(list_):
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice(list_, 1) == [2, 3]
     assert arrs.my_slice([], 0,-1) == []
     assert arrs.my_slice([1, 2, 3], -1,0) == []
-    assert arrs.my_slice([1, 2, 3], -4, 0) == []
+    assert arrs.my_slice(list_, -4, 0) == []
